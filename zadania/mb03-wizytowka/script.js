9bc8261840608e89246e0b348f8c3ae3c93ae1d1
unknown <user@V02E.edu2.zsl.poznan.pl>
2026-09-23 08:13:20 +0200

    generowanie listy umiejętności

---
+++ zadania/mb03-wizytowka/script.js
@@ -0,0 +1,20 @@
+const umiejetnosci = [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "SQL",
+    "Git",
+    "Praca w zespole"
+];
+
+function pokazUmiejetnosci(lista){
+    const kontener = document.querySelector("#lista-umiejetnosci");
+
+    for(const nazwa of lista){
+        const element = document.createElement("li");
+        element.textContent = nazwa;
+        kontener.appendChild(element);
+    }
+}
+
+pokazUmiejetnosci(umiejetnosci)
